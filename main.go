c78f740fb32c85b630c59bd1a2f4d92107ee2853
360ied <19516527+360ied@users.noreply.github.com>
2021-10-05 21:40:24 -0400

    make everything 10000000% better

--- main.go
+++ main.go
@@ -1,5 +1,59 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"notetest/notes"
+	"os"
+
+	_ "embed"
+
+	"golang.org/x/crypto/argon2"
+)
+
 func main() {
+	//go:embed usage.txt
+	var usage string
+
+	args := os.Args
+
+	if len(args) != 2 {
+		// too little args
+		_, _ = os.Stdout.WriteString("Too little or too many arguments.\n")
+		_, _ = os.Stdout.WriteString(usage)
+		os.Exit(2)
+	}
+
+	filePath := args
+
+	_, _ = os.Stdout.WriteString("Enter password: ")
+
+	r := bufio.NewReader(os.Stdin)
+	buf := &bytes.Buffer{}
+
+	for {
+		b, err := r.ReadByte()
+		if err != nil {
+			panic(err)
+		}
+
+		if b != '\n' {
+			buf.WriteByte(b)
+		} else {
+			break
+		}
+	}
+
+	key := argon2.IDKey(buf.Bytes(), make([]byte, 32), 1, 64*1024, 2, 32)
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	nm, err := notes.UnlockDB(file, key)
+	if err != nil {
+		panic(err)
+	}
 
 }
